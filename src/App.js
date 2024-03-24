47962e00c0cd2e1a146d30fc4e3bbf4c3cdfd688
stfrancoc <stfrancoc@gmail.com>
2024-03-24 16:48:00 -0500

    other try

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/uberPythones/build" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/requirements" element={<Requirements/>}/>
       </Routes>
